2253b604a9613da87138c83b6894ee53613a70bf
rcsyg <jurijs.grabovskis@gmail.com>
2020-06-05 20:22:35 +0300

    Lection 2 | part 1

---
+++ src/Variables.java
@@ -0,0 +1,41 @@
+public class Variables {
+
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 2;
+
+        System.out.println(x);
+        System.out.println(y);
+
+        x = 4;
+
+        System.out.println(x);
+        System.out.println(y);
+
+        int z;
+        z = 12;
+
+        byte b = 8;
+        short sh = 1000;
+        long veryBigNumber = 100000000000000L;
+
+        System.out.println(b);
+        System.out.println(sh);
+        System.out.println(veryBigNumber);
+
+        double decimal = 2.5d;
+        System.out.println(decimal);
+
+        boolean todayIsFriday = true;
+        boolean todayIsSunday = false;
+        System.out.println(todayIsFriday);
+        System.out.println(todayIsSunday);
+
+        char c = 'Z';
+        System.out.println(c);
+
+        String s = "Hello World!";
+        System.out.println(s);
+    }
+
+}
