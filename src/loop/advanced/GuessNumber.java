bbb5f7cb1ed3f64b041c6f4340a88de3a6f23dca
rcsyg <jurijs.grabovskis@gmail.com>
2020-06-19 18:55:16 +0300

    GuessNumber base added

---
+++ src/loop/advanced/GuessNumber.java
@@ -0,0 +1,21 @@
+package loop.advanced;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessNumber {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int r = new Random().nextInt(10) + 1;
+        boolean userGuessed = false;
+
+        while(!userGuessed) {
+
+        }
+
+        sc.close();
+    }
+
+}
--- src/loop/basic/Fibonacci.java
+++ src/loop/basic/Fibonacci.java
@@ -1,12 +1,35 @@
 package loop.basic;
 
+import java.util.Scanner;
+
 public class Fibonacci {
 
     public static void main(String[] args) {
-        // 0 1 1 2 3 5 8 13 21 ...
+        // 0   1   1    2    3 5 8 13 21 ...
+        // f1 f2
+        //    f1  f2
+        //        f1   f2
 
         int f1 = 0;
         int f2 = 1;
+        int sum = 0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number:");
+        int number = sc.nextInt();
+        sc.close();
+
+        System.out.print(f1 + " " + f2 + " ");
+
+        int i = 2;
+        while (i <= number) {
+            sum = f1 + f2;
+            System.out.print(sum + " ");
+            f1 = f2;
+            f2 = sum;
+            i++;
+        }
+
     }
 
 }
