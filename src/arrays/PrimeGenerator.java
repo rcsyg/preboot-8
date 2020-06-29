51b60d2e4cac2817eb97663e4a1f0d8da8d4afbf
rcsyg <jurijs.grabovskis@gmail.com>
2020-06-29 21:37:06 +0300

    1D arrays, continue

--- src/arrays/PrimeGenerator.java
+++ src/arrays/PrimeGenerator.java
@@ -1,5 +1,7 @@
 package arrays;
 
+import java.util.Scanner;
+
 public class PrimeGenerator {
 
     public static void main(String[] args) {
@@ -25,6 +27,35 @@ public class PrimeGenerator {
         * last) print results
         * */
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter primes to generate:");
+        int primesToGenerate = sc.nextInt();
+        sc.close();
+
+        int[] primes = new int[primesToGenerate];
+        int resultIndex = 0;
+        int candidate = 2;
+
+        while (resultIndex < primesToGenerate) {
+
+            boolean isPrime = true;
+            /*for (int i = 2; i < number / 2; i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }*/
+            if(isPrime) {
+                /*
+                * put [candidate] in resulting array
+                * */
+                resultIndex++;
+            }
+
+            candidate++;
+        }
+
+        // last) print results
     }
 
 }
